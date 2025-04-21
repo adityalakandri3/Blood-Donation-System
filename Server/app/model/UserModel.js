7faed2c40b8554072059590cc2990df614e9d170
adityalakandri3 <adityalakandri3@gmail.com>
2025-04-21 10:49:20 +0530

    Added UserModel

---
+++ Server/app/model/UserModel.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["donor", "recipient", "admin"],
+      default: "recipient",
+    },
+    bloodType: {
+      type: String,
+      required :true,
+    },
+    location: {
+        state: {
+          type: String,
+          required: true,
+        },
+        city: {
+          type: String,
+          required: true,
+        },
+      }
+  },
+  { timestamps: true }
+);
+
+
+const UserModel = mongoose.model('users',UserSchema);
+module.exports = UserModel;
