846c80307a6307e7147da0d2eeb347ab45e4c202
KumarRahul-01 <baitharahulkumar23@gmail.com>
2025-04-24 12:11:39 +0530

    blood request route

--- Server/app/router/BloodRequestRouter.js
+++ Server/app/router/BloodRequestRouter.js
@@ -1,17 +1,18 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const bloodRequestController = require('../controller/BloodRequest');
+const BloodRequestController = require('../controller/BloodRequest');
+const { AuthCheck } = require('../middleware/Auth');
 
 // Create a blood request
-router.post('/blood-requests', bloodRequestController.createBloodRequest);
+router.post('/blood-requests',AuthCheck, BloodRequestController.createBloodRequest);
 
 // Get all blood requests
-router.get('/blood-requests', bloodRequestController.getAllBloodRequests);
+router.get('/get-blood-request', AuthCheck,BloodRequestController.getAllBloodRequests);
 
 // Update blood request status
-router.put('/blood-requests/:id', bloodRequestController.updateBloodRequestStatus);
+router.put('/blood-requests/:id', BloodRequestController.updateBloodRequestStatus);
 
 // Get blood requests by status
-router.get('/blood-requests/status/:status', bloodRequestController.getBloodRequestsByStatus);
+router.get('/blood-requests/status/:status', BloodRequestController.getBloodRequestsByStatus);
 
 module.exports = router;
