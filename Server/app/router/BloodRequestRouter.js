e255908fe2b7500d44dd7597a6b3780c9ffe6c6b
adityalakandri3 <adityalakandri3@gmail.com>
2025-04-25 18:02:06 +0530

    Blood request Check and Completed

--- Server/app/router/BloodRequestRouter.js
+++ Server/app/router/BloodRequestRouter.js
@@ -1,31 +1,44 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const BloodRequestController = require('../controller/BloodRequest');
-const { AuthCheck, RoleCheck } = require('../middleware/Auth');
+const BloodRequestController = require("../controller/BloodRequest");
+const { AuthCheck, RoleCheck } = require("../middleware/Auth");
 
 // Create a blood request
-router.post('/create-blood-request',AuthCheck, RoleCheck('recipient'),BloodRequestController.createBloodRequest);
+router.post(
+  "/create-blood-request",
+  AuthCheck,
+  RoleCheck("recipient"),
+  BloodRequestController.createBloodRequest
+);
 
 // Get all blood requests
-router.get('/get-blood-request', AuthCheck,BloodRequestController.getAllBloodRequests);
+router.get(
+  "/get-blood-request",
+  AuthCheck,
+  BloodRequestController.getAllBloodRequests
+);
 
-<<<<<<< HEAD
 //get blood request by id
-router.get('/get-blood-request/:id', AuthCheck,BloodRequestController.getAllBloodRequestById);
+router.get(
+  "/get-blood-request/:id",
+  AuthCheck,
+  BloodRequestController.getAllBloodRequestById
+);
 
 // Update blood request
-router.put('/update-blood-request/:id', AuthCheck, RoleCheck('recipient'),BloodRequestController.updateBloodRequest);
+router.put(
+  "/update-blood-request/:id",
+  AuthCheck,
+  RoleCheck("recipient"),
+  BloodRequestController.updateBloodRequest
+);
 
 //delete blood request by id
-router.delete('/delete-blood-request/:id', AuthCheck, RoleCheck('recipient'),BloodRequestController.deleteBloodRequest);
-
-
-=======
-// Update blood request status
-router.put('/blood-requests/:id',AuthCheck, BloodRequestController.updateBloodRequestStatus);
-
-// Get blood requests by status
-router.get('/blood-requests/status/:status',AuthCheck, BloodRequestController.getBloodRequestsByStatus);
->>>>>>> e4f96f2e10a1ce239242b5a9e065dbe2cb227f34
+router.delete(
+  "/delete-blood-request/:id",
+  AuthCheck,
+  RoleCheck("recipient"),
+  BloodRequestController.deleteBloodRequest
+);
 
 module.exports = router;
