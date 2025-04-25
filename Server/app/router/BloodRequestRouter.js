ab51d3b33a133b8cebecc8fb4b106072e949254b
KumarRahul-01 <baitharahulkumar23@gmail.com>
2025-04-25 13:49:28 +0530

    all check

--- Server/app/router/BloodRequestRouter.js
+++ Server/app/router/BloodRequestRouter.js
@@ -10,9 +10,9 @@ router.post('/blood-requests',AuthCheck, BloodRequestController.createBloodReque
 router.get('/get-blood-request', AuthCheck,BloodRequestController.getAllBloodRequests);
 
 // Update blood request status
-router.put('/blood-requests/:id', BloodRequestController.updateBloodRequestStatus);
+router.put('/blood-requests/:id',AuthCheck, BloodRequestController.updateBloodRequestStatus);
 
 // Get blood requests by status
-router.get('/blood-requests/status/:status', BloodRequestController.getBloodRequestsByStatus);
+router.get('/blood-requests/status/:status',AuthCheck, BloodRequestController.getBloodRequestsByStatus);
 
 module.exports = router;
